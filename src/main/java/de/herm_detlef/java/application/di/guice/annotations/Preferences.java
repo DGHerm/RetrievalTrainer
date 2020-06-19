05c86dfddae35f70170802aa4727143f6d0b01e5
Detlef Gregor Herm <detlef.g.herm@posteo.de>
2020-06-19 08:12:13 +0200

    Google Guice, binding annotation: -- restricted the target to PARAMETER

--- src/main/java/de/herm_detlef/java/application/di/guice/annotations/Preferences.java
+++ src/main/java/de/herm_detlef/java/application/di/guice/annotations/Preferences.java
@@ -20,8 +20,8 @@ import javax.inject.Qualifier;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Qualifier @Target({ FIELD, PARAMETER, METHOD }) @Retention(RUNTIME)
+@Qualifier @Target({ PARAMETER }) @Retention(RUNTIME)
 public @interface Preferences {}
